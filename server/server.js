2412bd211aa7a3c408aa50545e1b79c09f911943
Juan Pablo Pieroni <seisduro@gmail.com>
2025-08-27 14:08:41 -0300

    fix: remove trailing slash from allowed origin URL for CORS

--- server/server.js
+++ server/server.js
@@ -20,7 +20,7 @@ app.use(express.static(path.join(__dirname, "dist")));
 // 🟣 Allowed origins (dev + futuro deploy)
 const allowedOrigins = [
   "http://localhost:5173",              // desarrollo local
-  "https://generaladeutsche.netlify.app/"           // poner la URL real
+  "https://generaladeutsche.netlify.app"           // poner la URL real
 ]
 // ⚠️ CORS para permitir desde el frontend
 app.use(
